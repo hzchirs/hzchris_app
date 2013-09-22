af35b4b53b4ab1eb3296f316351a2df0500560f6
HzChris <hzchirs@gmail.com>
2013-09-22 20:14:39 +0800

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HzchrisApp::Application.config.secret_key_base = '4352a38c412d6c014718bdb85ad575f3c4927879207f5583baebd478be8b3361dc01ddec7ae19e5a26728e8597e5a30b2ff9e669a91ca20c75adfd3e8da1dcec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HzchrisApp::Application.config.session_store :cookie_store, key: '_hzchris_app_session'
